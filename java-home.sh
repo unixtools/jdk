d302f46f72031d16081dcfa1757bc5807e7d72d9
Nathan Neulinger <nneul@neulinger.org>
2017-12-04 14:12:58 -0600

    Remove old JDK 1.6 support

--- java-home.sh
+++ java-home.sh
@@ -1,9 +1,4 @@
-if [ -e /local/jdk/jdk1.6 ]; then
-	export JAVA_HOME=/local/jdk/jdk1.6
+if [ -e /local/jdk/latest ]; then
+	export JAVA_HOME=/local/jdk/latest
 	export PATH=${JAVA_HOME}/bin:${PATH}
-else
-	if [ -e /local/jdk/latest ]; then
-		export JAVA_HOME=/local/jdk/latest
-		export PATH=${JAVA_HOME}/bin:${PATH}
-	fi
 fi
